c64543b21bc0976352fabc80fc50f3ef082e3ac7
ross0738 <ross0738@d.umn.edu>
2023-11-01 15:31:10 -0500

    Added the new reminders page and started it so we can make reminders.

--- lib/reminder_screen.dart
+++ lib/reminder_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'create_reminder.dart';
+
 class ReminderScreen extends StatefulWidget {
   @override
   _ReminderScreen createState() => _ReminderScreen();
@@ -11,7 +13,35 @@ class _ReminderScreen extends State<ReminderScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Reminders')),
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
+
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(32),
+
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 0.0, bottom: 20.0),
+              child: Text("Reminders",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent), textScaleFactor: 2,)
+              ,),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.blueAccent,
+                  padding: EdgeInsets.all(10.0),
+
+              ),
+              child: Text('New Reminder'),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                  return CreateReminder();
+                }));
+              },
+            ),
+          ],
+        ),
+      ),
+
     );
   }
 }
