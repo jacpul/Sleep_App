60c88b57f277dbcfce723a3a2d7404bdda6ecf3b
ross0738 <ross0738@d.umn.edu>
2023-11-02 15:10:16 -0500

    Working on creating a reminder page, added some UI elements

--- lib/create_reminder.dart
+++ lib/create_reminder.dart
@@ -7,12 +7,49 @@ class CreateReminder extends StatefulWidget {
 }
 
 class _CreateReminder extends State<CreateReminder> {
+  int currentSelectedTime = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Create a Reminder')),
       backgroundColor: Colors.white,
 
+      body: Container(
+        color: Colors.yellowAccent.shade100,
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Radio<int>(
+                activeColor: Colors.blueAccent.shade400,
+                value: 0, groupValue: currentSelectedTime, onChanged: (value) {
+                    setState(() {
+                      currentSelectedTime = 1;
+                    });
+                }
+              ),
+              Text(
+                'AM',
+                style: TextStyle(color: Colors.blueAccent.shade400),
+              ),
+              Radio<int>(
+                activeColor: Colors.blueAccent.shade400,
+                value: 1, groupValue: currentSelectedTime, onChanged: (value) {
+                  setState(() {
+                    currentSelectedTime = 0;
+                  });
+                }
+              ),
+              Text(
+                'PM',
+                style: TextStyle(color: Colors.blueAccent.shade400),
+              ),
+            ],
+          ),
+      ),
     );
   }
 }
