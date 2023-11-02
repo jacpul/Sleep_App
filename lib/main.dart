eabd9f632799d5c20692a54edc11fcef51937f75
Jordan Edel <edel0071@d.umn.edu>
2023-11-02 13:47:47 -0500

    resloved merge errors

--- lib/main.dart
+++ lib/main.dart
@@ -12,152 +12,69 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Builder(
-            builder: (context) => Scaffold(
+      home: Builder(
+        builder: (context) =>
+            Scaffold(
               appBar: AppBar(
                 title: Text("Login"),
                 backgroundColor: Colors.deepOrange,
               ),
               body: Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(32),
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/sun.jpg'),
-                      fit: BoxFit.cover,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(32),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/sun.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                      child: Text(
+                        "Login",
+                        style: const TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent),
+                        textScaleFactor: 3,
+                      ),
                     ),
 
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                        child: Text("Login",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent), textScaleFactor: 3,)
-                        ,),
-
-                      // Button to bring you to the calculate page
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.blueAccent
-                        ),
-                        child: Text('Login'),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                            return Home();
-                          }));
-                        },
+                    // Username Textfield
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Username'),
                       ),
-                    ],
-                  )
+                    ),
+
+                    // Password Textfield
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        obscureText: true, // Hide the password input
+                      ),
+                    ),
+
+                    // Button to bring you to the main page
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.blueAccent),
+                      child: Text('Login'),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                              return Home();
+                            }));
+                      },
+                    ),
+                  ],
+                ),
               ),
 
-        home: Builder(
-        builder: (context) => Scaffold(
-          appBar: AppBar(
-            title: Text("Omega Dreams", style: const TextStyle(color: Colors.blueAccent,
-                                                                fontWeight: FontWeight.bold,
-                                                                fontSize: 35)),
-            centerTitle: true,
-            backgroundColor: Colors.deepOrange,
-          ),
-          body: Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(32),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/sun.jpg'),
-                fit: BoxFit.cover,
-            ),
-
-          ),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, bottom: 60.0),
-                  child: Text("Sweet Dreams!",style: const TextStyle(fontWeight: FontWeight.bold,
-                                                                      color: Colors.yellow),
-                                                                      textScaleFactor: 3,)
-                  ,),
-
-
-
-                // Button to bring you to the calculate page
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(150.0, 50.0),
-                      primary: Colors.blueAccent,
-                  ),
-                  child: Text('Log Your Sleep', style: const TextStyle(fontWeight: FontWeight.bold,
-                                                                        fontSize: 18,
-                                                                        color: Colors.orange),
-                                                                        textAlign: TextAlign.center),
-
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                      return SplashScreen();
-                    }));
-                  },
-                ),
-
-
-
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                ),
-
-
-
-
-                // Button to bring you to your calendar page
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(150.0, 50.0),
-                      primary: Colors.blueAccent
-                  ),
-                  child: Text('Your Calendar', style: const TextStyle(fontWeight: FontWeight.bold,
-                                                                      fontSize: 18,
-                                                                      color: Colors.orange),
-                                                                      textAlign: TextAlign.center),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                      //return SplashScreen();
-                      return new CalendarScreen();
-                    }));
-                  },
-                ),
-
-
-
-
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                ),
-
-
-
-
-                // Button to bring you to your notifications page
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(150.0, 50.0),
-                      primary: Colors.blueAccent
-                  ),
-                  child: Text('Your Notifications', style: const TextStyle(fontWeight: FontWeight.bold,
-                                                                            fontSize: 18,
-                                                                            color: Colors.orange),
-                                                                            textAlign: TextAlign.center),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                      return NotificationScreen();
-                    }));
-                  },
-                )
-              ],
-            )
-        )
-    )
-    )
-    )
+        //here
+    ),
     )
     );
   }
@@ -298,4 +215,5 @@ class _Home extends State<Home>{
     );
   }
 }
+
 
