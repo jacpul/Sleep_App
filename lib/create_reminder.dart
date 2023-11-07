5e27ca8a614fd79103f8d2a783da99b3c7217e17
ross0738 <ross0738@d.umn.edu>
2023-11-07 14:13:13 -0600

    Edited the UI for the reminders page

--- lib/create_reminder.dart
+++ lib/create_reminder.dart
@@ -9,6 +9,11 @@ class CreateReminder extends StatefulWidget {
 class _CreateReminder extends State<CreateReminder> {
   int currentSelectedTime = 0;
 
+  var _ReminderHourController = TextEditingController();
+  var _ReminderMinuteController = TextEditingController();
+  String _ReminderMinute = "0.0";
+  String _ReminderHour = "0.0";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,9 +52,75 @@ class _CreateReminder extends State<CreateReminder> {
                 'PM',
                 style: TextStyle(color: Colors.blueAccent.shade400),
               ),
+              Container(
+                  color: Colors.yellowAccent.shade100,
+                  margin: EdgeInsets.all(8.0),
+                  padding: EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: reminderHourFormField(context),
+                            ),
+                            Expanded(
+                              child: reminderMinuteFormField(context),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+              ),
             ],
           ),
       ),
     );
   }
+
+  TextFormField reminderHourFormField(BuildContext context) {
+    return TextFormField(
+      controller: _ReminderHourController,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (value!.length == 0 || (double.parse(value) < 1 || double.parse(value) > 12)) {
+          return ('Hour between 1 - 12');
+        }
+      },
+      onSaved: (value) {
+        _ReminderHour = value!;
+      },
+      decoration: InputDecoration(
+        hintText: "e.g.) 7",
+        labelText: "Hour",
+        icon: Icon(Icons.access_time_rounded),
+        fillColor: Colors.white,
+      ),
+    );
+  }
+
+  TextFormField reminderMinuteFormField(BuildContext context) {
+    return TextFormField(
+      controller: _ReminderMinuteController,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.done,
+      validator: (value) {
+        if (value!.length == 0 || (double.parse(value) < 0 || double.parse(value) > 59)) {
+          return ('Minute between 0 - 59');
+        }
+      },
+      onSaved: (value) {
+        _ReminderMinute = value!;
+      },
+      decoration: InputDecoration(
+          hintText: 'e.g.) 40',
+          labelText: 'Minute',
+          icon: Icon(Icons.access_time_rounded),
+          fillColor: Colors.white
+      ),
+    );
+  }
+
 }
