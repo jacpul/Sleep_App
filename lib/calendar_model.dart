a931f353289716ce11202c30bc78882f0edae8e9
teddy j <jedde020@d.umn.edu>
2023-11-18 18:55:24 -0600

    4 hours of messing with firebase :D still not working... yet

--- lib/calendar_model.dart
+++ lib/calendar_model.dart
@@ -1,33 +1,22 @@
 import 'dart:collection';
 import 'package:table_calendar/table_calendar.dart';
 
-final kToday = DateTime.now();
-final kFirstDay = DateTime(kToday.year, kToday.month - 5, kToday.day);
-final kLastDay = DateTime(kToday.year, kToday.month + 5, kToday.day);
+final eventToday = DateTime.now();
+final eventFirstDay = DateTime(eventToday.year, eventToday.month - 5, eventToday.day);
+final eventLastDay = DateTime(eventToday.year, eventToday.month + 5, eventToday.day);
 
 class Event {
   final String title;
-  const Event(this.title);
+  final String day;
+  final String month;
+  final String year;
+
+  Event(this.title, this.day, this.month, this.year);
 
   @override
   String toString() => title;
 }
 
-LinkedHashMap<DateTime, List<Event>>kEvents = LinkedHashMap<DateTime, List<Event>>(
-  equals: isSameDay,
-  hashCode: getHashCode,
-)..addAll(_kEventSource);
-
 int getHashCode(DateTime key) {
 return key.day * 1000000 + key.month * 10000 + key.year;
 }
-
-Map<DateTime, List<Event>> _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
-  key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-  value: (item) => List.generate(
-      item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-  ..addAll({
-  kToday: [
-    Event('Today\'s Event 1'),
-  ],
-});
--- lib/calendar_screen.dart
+++ lib/calendar_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:collection';
+import 'dart:core';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:units/reminder_screen.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'splash_screen.dart';
 import 'main_screen.dart';
 
@@ -19,153 +24,187 @@ class _CalendarScreen extends State<CalendarScreen> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   late final ValueNotifier<List<Event>> _selectedEvents;
+  late String currentUser;
+  late CollectionReference logCollection = FirebaseFirestore.instance.collection('users').doc(currentUser).collection('Logs');
 
   @override
   void initState() {
     super.initState();
 
+    currentUser = FirebaseAuth.instance.currentUser!.uid;
     _selectedDay = _focusedDay;
-    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!) as List<Event>);
+  }
+
+  Future<LinkedHashMap<DateTime, List<Event>>> fetchAndGroupEvents(String currentUser) async {
+    Map<DateTime, List<Event>> eventStorage = await getEvents(currentUser);
+    return LinkedHashMap<DateTime, List<Event>>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(eventStorage);
+  }
+
+  Future<Map<DateTime, List<Event>>> getEvents(String currentUser) async {
+    Map<DateTime, List<Event>> eventStorage = {};
+    QuerySnapshot<Map<String, dynamic>> querySnapshot =
+    await FirebaseFirestore.instance.collection('users').doc(currentUser).collection('Logs').get();
+
+    for (QueryDocumentSnapshot<Map<String, dynamic>> documentSnapshot in querySnapshot.docs) {
+      Map<String, dynamic> logData = documentSnapshot.data();
+      String title = "Test";
+      String day = logData['Day'];
+      String month = logData['Month'];
+      String year = logData['Year'];
+      String hourSlept = logData['Hours_Slept'];
+
+      DateTime date = DateTime(int.parse(year), int.parse(month), int.parse(day));
+      Event event = Event(title, day, month, year);
+      if (eventStorage[date] == null) {
+        eventStorage[date] = [];
+      }
+      eventStorage[date]!.add(event);
+    }
+    return eventStorage;
   }
 
   List<Event> _getEventsForDay(DateTime day) {
-    // Implementation example
-    return kEvents[day] ?? [];
+    LinkedHashMap<DateTime, List<Event>> eventStorage = fetchAndGroupEvents(currentUser) as LinkedHashMap<DateTime, List<Event>>;
+    return eventStorage[day] ?? [];
   }
 
-  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+  Future<void> _onDaySelected(DateTime selectedDay, DateTime focusedDay) async {
     if (!isSameDay(_selectedDay, selectedDay)) {
       setState(() {
         _selectedDay = selectedDay;
         _focusedDay = focusedDay;
       });
 
-      _selectedEvents.value = _getEventsForDay(selectedDay);
+      _selectedEvents.value = _getEventsForDay(selectedDay) as List<Event>;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        //title: Text("Calendar"),
-          backgroundColor: Colors.deepOrangeAccent,
-        actions: [ //appbar functions
+        appBar: AppBar(
+          //title: Text("Calendar"),
+            backgroundColor: Colors.deepOrangeAccent,
+            actions: [ //appbar functions
 
-          //Home button
-          IconButton(
-            icon:const Icon(Icons.add_home_outlined),
-            tooltip: "Home",
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (BuildContext context) {
-                    return Home();
+              //Home button
+              IconButton(
+                icon:const Icon(Icons.add_home_outlined),
+                tooltip: "Home",
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (BuildContext context) {
+                        return Home();
+                      }));
+                },
+              ),
+
+              //log button
+              IconButton(
+                icon: const Icon(Icons.mode_edit_outlined),
+                tooltip: 'Log',
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return SplashScreen();
                   }));
-            },
-          ),
+                },
+              ),
 
-          //log button
-          IconButton(
-            icon: const Icon(Icons.mode_edit_outlined),
-            tooltip: 'Log',
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                return SplashScreen();
-              }));
-            },
-          ),
+              // Calendar Button
+              IconButton(
+                icon: const Icon(Icons.calendar_month),
+                tooltip: 'Calendar',
+                onPressed: () {
+                  // returns nothing, already in calendar
+                },
+              ),
 
-          // Calendar Button
-          IconButton(
-            icon: const Icon(Icons.calendar_month),
-            tooltip: 'Calendar',
-            onPressed: () {
-              // returns nothing, already in calendar
-              },
-          ),
+              //Notifications Button
+              IconButton(
+                icon: const Icon(Icons.new_releases_outlined),
+                tooltip: 'Notifications',
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return NotificationScreen();
+                  }));
+                },
+              ),
 
-          //Notifications Button
-          IconButton(
-            icon: const Icon(Icons.new_releases_outlined),
-            tooltip: 'Notifications',
-            onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-              return NotificationScreen();
-            }));
-          },
-          ),
+              //Calendar Button
 
-          //Calendar Button
+              //Reminder Button
+              IconButton(
+                icon: const Icon(Icons.add_alert_outlined),
+                tooltip: 'Reminders',
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return ReminderScreen();
+                  }));
+                },
+              )
+            ]
+        ),
+        backgroundColor: Colors.yellow.shade800,
 
-          //Reminder Button
-          IconButton(
-            icon: const Icon(Icons.add_alert_outlined),
-            tooltip: 'Reminders',
-            onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-              return ReminderScreen();
-            }));
-          },
-          )
-        ]
-      ),
-      backgroundColor: Colors.yellow.shade800,
-
-      body: Column(
-        children: [
-          TableCalendar<Event>(
-            firstDay: kFirstDay,
-            lastDay: kLastDay,
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            eventLoader: _getEventsForDay,
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: _onDaySelected,
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-            // Call `setState()` when updating calendar format
-              setState(() {
-                _calendarFormat = format;
-              });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              // No need to call `setState()` here
-              _focusedDay = focusedDay;
-            },
-          ),
-          const SizedBox(height: 7.0),
-          Expanded(
-            child: ValueListenableBuilder<List<Event>>(
-              valueListenable: _selectedEvents,
-              builder: (context, value, _) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 10.0,
-                        vertical: 4.0,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: ListTile(
-                        onTap: () => print('${value[index]}'),
-                        title: Text('${value[index]}'),
-                      ),
+        body: Column(
+            children: [
+              TableCalendar<Event>(
+                firstDay: eventFirstDay,
+                lastDay: eventLastDay,
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                eventLoader: _getEventsForDay,
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: _onDaySelected,
+                onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    // Call `setState()` when updating calendar format
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                },
+                onPageChanged: (focusedDay) {
+                  // No need to call `setState()` here
+                  _focusedDay = focusedDay;
+                },
+              ),
+              const SizedBox(height: 7.0),
+              Expanded(
+                child: ValueListenableBuilder<List<Event>>(
+                  valueListenable: _selectedEvents,
+                  builder: (context, value, _) {
+                    return ListView.builder(
+                      itemCount: value.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 10.0,
+                            vertical: 4.0,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: ListTile(
+                            onTap: () => print('${value[index]}'),
+                            title: Text('${value[index]}'),
+                          ),
+                        );
+                      },
                     );
                   },
-                );
-              },
-            ),
-          ),
-        ]
-      )
+                ),
+              ),
+            ]
+        )
     );
   }
 }
