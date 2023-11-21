be4d834448da7d84194a0c56f89262a8eb672c98
ross0738 <ross0738@d.umn.edu>
2023-11-21 16:26:35 -0600

    Fixed a minor merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:units/login.dart';
 import 'main_screen.dart';
 import 'api/firebase_api.dart'; // notifications
+import 'notification_screen.dart'; // notifications
+import 'package:timezone/data/latest.dart' as tz; // timed notifications
 
 
 // Used for navigating between notifications
 final navigatorKey = GlobalKey<NavigatorState>();
-
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +22,21 @@ void main() async {
     ),
   );
   await FirebaseApi().initNotifications();
-  runApp(Loginpage());
-  //runApp(Home());
+  tz.initializeTimeZones();
+  runApp(MyApp());
+}
+
+// Run MyApp so we are able to use the navigator everywhere in the app
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      routes: {
+        NotificationScreen.route: (context) => NotificationScreen()
+      },
+      home: Loginpage(),
+    );
+  }
 }
 
